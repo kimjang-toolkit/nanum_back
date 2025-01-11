a749ea8592d72f88493b5e7bd28e2e7a9d04c404
ohchansol <haxr369@gmail.com>
2025-01-11 14:38:10 +0900

    Create get 공구글 리스트 api 만들기 시작

--- nanum_back/event-func/cobuying/get.ts
+++ nanum_back/event-func/cobuying/get.ts
@@ -36,3 +36,59 @@ export const getCoBuyingByIdHandler = async (event: APIGatewayProxyEvent): Promi
         };
     }
 };
+
+/**
+ *
+ * Post ->
+ * 정렬 기준 : 생성일자순[내림차순] | 마감순[오름차순]
+ * 정렬 순서 : 오름차순, 내림차순
+ * 필터 조건 : 공구상태, 공구장이름, 생성일자, 마감일자
+ *
+ * @param event
+ * @returns
+ */
+export const getCoBuyingListHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    try {
+        const { ownerName, createdAt, id } = event.queryStringParameters ?? {};
+
+        if (!ownerName || !createdAt || !id) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({
+                    message: '찾고자하는 공구글을 입력해주세요.',
+                }),
+            };
+        }
+        if (!event.body) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ message: '요청 본문이 비어있습니다.' }),
+            };
+        }
+
+        const input: string = JSON.parse(event.body);
+        // 본문 확인
+        if (!event.body) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ message: '요청 본문이 비어있습니다.' }),
+            };
+        }
+
+        console.log(' ownerName : ' + ownerName + '\n createdAt : ' + createdAt + '\n id : ' + id);
+        const cobuying = await queryCoBuyingById(ownerName, createdAt, id);
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(cobuying),
+        };
+    } catch (err) {
+        console.error(err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: err,
+            }),
+        };
+    }
+};
